fc7054884c4f62e93ada05d198616194f14cb194
JRJamesCJ <loriegapogi03@gmail.com>
2026-03-12 17:56:51 +0800

    starting modals working basic !! bombaclat:

--- src/main.js
+++ src/main.js
@@ -3,12 +3,59 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import gsap from "gsap"
 
 const canvas = document.querySelector('#experience-canvas');
 const sizes ={
     width: window.innerWidth,
     height: window.innerHeight,
 };
+
+const modals = {
+    work: document.querySelector(".modal.work"),
+    about: document.querySelector(".modal.about"),
+    contact: document.querySelector(".modal.contact"),
+};
+
+document.querySelectorAll(".modal-exit-button").forEach((button) =>{
+    button.addEventListener("click", (e) =>{
+        const modal = e.target.closest(".modal");
+        hideModal(modal);
+    });
+});
+
+const showModal = (modal) => {
+    modal.style.display = "block"
+
+    gsap.set(modal, {opacity: 0 });
+
+    gsap.to(modal, {
+        opacity:1,
+        duration:0.5,
+    });
+};
+
+const hideModal = (modal) => {
+    gsap.to(modal, {
+        opacity: 0,
+        duration: 0.5,
+        onComplete: () =>{
+            modal.style.display = "none";
+        }
+    });
+};
+
+const raycasterObjects = [];
+let currentIntersects = [];
+
+const socialLinks = {
+    Instagram: "https://www.instagram.com/",
+    Facebook: "https://www.facebook.com/",
+    Youtube: "https://www.youtube.com/",
+};
+
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
 
 // Loaders
 const textureLoader = new THREE.TextureLoader();
@@ -74,13 +121,45 @@ videoElement.loop = true;
 videoElement.muted = true;
 videoElement.playsInline = true;
 videoElement.autoplay = true;
-videoElement.play()
+videoElement.play();
 
-const videoTexture = new THREE.VideoTexture(videoElement)
+const videoTexture = new THREE.VideoTexture(videoElement);
 videoTexture.colorSpace = THREE.SRGBColorSpace;
 videoTexture.flipY = false;
 
-loader.load("/models/Room_Portfolio_V2.glb", (glb)=> {
+window.addEventListener("mousemove", (e) => {
+    pointer.x = (e.clientX / window.innerWidth) * 2 - 1;
+    pointer.y = -(e.clientY / window.innerHeight) * 2 + 1;
+});
+window.addEventListener("click", (e) => {
+    if (currentIntersects.length> 0) {
+        const object = currentIntersects[0].object;
+
+        Object.entries(socialLinks). forEach(([key, url]) => {
+            if (object.name.includes(key)) {
+                const newWindow = window.open();
+                newWindow.opener = null;
+                newWindow.location = url;
+                newWindow.target = "_blank";
+                newWindow.rel = "noopener noreferrer";
+
+            }
+        });
+
+        if (object.name.includes("Work_Button")) {
+            showModal(modals.work)
+        } else if (object.name.includes("About_Button")){
+            showModal(modals.about)
+            
+        }else if (object.name.includes("Contact_Button")){
+            showModal(modals.contact)
+            
+        }
+
+    }
+});
+
+loader.load("/models/Room_Portfolio_V3.glb", (glb)=> {
     glb.scene.traverse((child) =>{
         if(child.isMesh){
             if (child.name.includes("Water")) {
@@ -112,6 +191,12 @@ loader.load("/models/Room_Portfolio_V2.glb", (glb)=> {
                         }
                     }
                 });
+
+
+            }
+            //  ✅ Add all meshes ending with "_Raycaster" to raycasterObjects
+             if (child.name.includes("Raycaster")) {
+                raycasterObjects.push(child);
 
             }
         }
@@ -166,6 +251,27 @@ const render = () =>{
     // console.log(camera.position);
     // console.log("000000000");
     // console.log(controls.target);
+
+    // Raycaster
+    raycaster.setFromCamera(pointer, camera);
+
+    // calculate objects intersecting the picking ray
+    currentIntersects = raycaster.intersectObjects(raycasterObjects);
+
+    for(let  i = 0 ; i < currentIntersects.length; i++) {
+    }
+
+    if(currentIntersects.length>0){
+        const currentIntersectsObject = currentIntersects[0].object
+
+        if(currentIntersectsObject.name.includes("Pointer")){
+            document.body.style.cursor = "pointer";
+        } else {
+            document.body.style.cursor = "default";
+        }
+    } else {
+        document.body.style.cursor = "default";
+    }
 
   renderer.render( scene, camera );
 
